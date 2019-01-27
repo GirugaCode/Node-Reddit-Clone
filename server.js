4330c33577b99edc4157cc584b958ae3236e72d9
GirugaCode <ryan.dphu.nguyen@gmail.com>
2019-01-26 22:49:56 -0800

    [add] Users can submit and save posts

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
 const port = 3000
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -9,5 +11,17 @@ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.render('home');
 });
+// Use Body Parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Add after body parser initialization
+app.use(expressValidator())
+
+require('./controllers/posts.js')(app)
+
+// Set db
+require('./data/reddit-db')
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
