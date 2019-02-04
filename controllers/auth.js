4ff2f850334810d95982424d8755c85628c8f980
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-02-04 13:09:46 -0800

    [add] Users can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,18 @@ const User = require("../models/user");
 module.exports = (app) => {
   //SIGN UP FORM
   app.get("/sign-up", (req, res) => {
-    res.render("sign-up");
+    res.render('sign-up');
+  })
+
+  //LOGOUT
+  app.get('/logout', (req,res) => {
+    res.clearCookie('nToken');
+    res.redirect('/');
+  })
+
+  //LOGIN FORM
+  app.get('/login', (req,res) => {
+    res.render('login')
   })
 
   //SIGN UP POST
@@ -24,4 +35,35 @@ module.exports = (app) => {
         return res.status(400).send({ err: err });
       });
   });
+
+  app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    // Find this user name
+    User.findOne({ username }, "username password")
+      .then(user => {
+        if (!user) {
+          // User not found
+          return res.status(401).send({ message: "Wrong Username or Password" });
+        }
+        // Check the password
+        user.comparePassword(password, (err, isMatch) => {
+          if (!isMatch) {
+            // Password does not match
+            return res.status(401).send({ message: "Wrong Username or password" });
+          }
+          // Create a token
+          const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+            expiresIn: "60 Days"
+          });
+          // Set a cookie and redirect to root
+          res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+          res.redirect("/");
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
 };
